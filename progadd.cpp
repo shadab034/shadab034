73c3aa0409238e68288c783c7cef0583a2b494fa
Shadab Hussain <shadabhussain034@gmail.com>
2023-01-05 01:06:08 +0530

    adding program added

---
+++ progadd.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10, b = 20;
+    int c = a + b;
+    cout << "Value of a & b are " << a << b << "respectively" << endl;
+    cout << "Sum of a & b is " << c;
+    return 0;
+}
